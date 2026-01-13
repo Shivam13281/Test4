9c62600ed5e6b316f1a403f6815075595f2fa747
Shivam3281@gmail.com <shivam7208@gmail.com>
2026-01-13 22:19:08 +0530

    new message

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test4Application {
-
-<<<<<<< HEAD
 	boolean x = true;
-
-=======
 	char ch = 'x';
->>>>>>> 4f1379d8e1d93aaf0e198308a77115ec07e9c9f9
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
 	}
