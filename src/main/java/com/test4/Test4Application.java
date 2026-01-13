37ca899b64af568bccc6e75bcc9cf235db0ff843
Shivam3281@gmail.com <shivam7208@gmail.com>
2026-01-13 22:09:27 +0530

    Added something new

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test4Application {
 
+	boolean x = true;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
 	}
